b9cbc22dfda6da26e5219ea457fac64166b2abfc
jimmywilbur <95513859+ibnumalikmudzopar@users.noreply.github.com>
2024-07-02 16:37:28 +0700

    on android move code home.dart ke sini

---
+++ lib/widget/barang.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BarangScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ini adalah halaman Barang'),
+    );
+  }
+}
+
+// move code home.dart ke sini
