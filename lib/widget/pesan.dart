6ba94615bf3c1a7d4514f21d7c2755c7052f2631
jimmywilbur <95513859+ibnumalikmudzopar@users.noreply.github.com>
2024-07-02 16:44:14 +0700

    Update widget

---
+++ lib/widget/pesan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PesanScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ini adalah halaman Pesan'),
+    );
+  }
+}
