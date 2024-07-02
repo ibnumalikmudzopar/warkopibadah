f6bca2d1a88bde3b4335d524faf7c1d0f4fc222c
jimmywilbur <95513859+ibnumalikmudzopar@users.noreply.github.com>
2024-07-02 16:43:58 +0700

    Update widget

---
+++ lib/widget/belanja.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BelanjaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ini adalah halaman Belanja'),
+    );
+  }
+}
