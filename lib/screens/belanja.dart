d7ccd7aaa28227207a70cdb7fe2d924c877e9bf0
jimmywilbur <95513859+ibnumalikmudzopar@users.noreply.github.com>
2024-07-13 21:37:46 +0700

    130724

--- lib/screens/belanja.dart
+++ lib/screens/belanja.dart
@@ -1,201 +1,141 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Add this import
+import 'package:intl/intl.dart';
 
 class BelanjaScreen extends StatefulWidget {
+  BelanjaScreen({Key? key}) : super(key: key);
+
   @override
   _BelanjaScreenState createState() => _BelanjaScreenState();
 }
 
 class _BelanjaScreenState extends State<BelanjaScreen> {
-  late PageController _pageController;
-  DateTime _currentDate = DateTime.now();
-
-  /*
-  -------------------------------------------------------------------------------------------------------------------
-  -----------------------------------------------------FIREBASE------------------------------------------------------
-  -------------------------------------------------------------------------------------------------------------------
-  */
-
-  // TableCell(child: Center(child: Padding(padding: const EdgeInsets.all(7.0), child: Text(item.hargapak),),)),
-  // item.id 
-  // add to belanja_items
-  // item.id,
-  // banyaknya: 2,
-  // tanggal: time.now()
-  /*
-/*
-1. buat collection belanja_items (items ganti jadi apalah)
-2. get item.id dari barang_items
-   add to belanja_items
-     item.id
-     jumlahBelanjaController...
-     tanggalController or dateTime.now()
-3. get belanja_items
-   show on tabel or ...
-*/
-  Metode untuk menambahkan barang baru ke Firestore
-  addItem(String name, String hargapcs, String hargapak, String kategori, String modal) {
-    var item = Item(id: 'id', name: name, hargapcs: hargapcs, hargapak: hargapak, kategori: kategori, modal: modal);
-    FirebaseFirestore.instance.collection(COLLECTION_NAME).add(item.toJson());
-  }
-  */
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(
-      initialPage: _currentDate.month - 1,
-    );
-  }
+  final List<String> daysOfWeek = ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'];
+  bool showForm = false; // Menyimpan status apakah form ditampilkan atau tidak
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String monthName = DateFormat.yMMMM().format(now);
+    DateTime firstDayOfMonth = DateTime(now.year, now.month, 1);
+    int startWeekday = firstDayOfMonth.weekday % 7; // 0 untuk Minggu, 1 untuk Senin, dst.
+
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _currentDate = DateTime(_currentDate.year, index + 1);
-          });
-        },
-        itemBuilder: (context, index) {
-          DateTime date = DateTime(_currentDate.year, index + 1);
-          return Column(
-            children: [
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              monthName,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: daysOfWeek.map((day) => Expanded(
+                child: Center(
+                  child: Text(day, style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              )).toList(),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 7,
+                ),
+                itemCount: daysInMonth(now) + startWeekday,
+                itemBuilder: (context, index) {
+                  if (index < startWeekday) {
+                    return Center(
+                      child: Text(''),
+                    );
+                  }
+                  DateTime date = DateTime(now.year, now.month, index - startWeekday + 1);
+                  bool isToday = date.day == now.day && date.month == now.month && date.year == now.year;
+                  return Center(
+                    child: Text(
+                      DateFormat.d().format(date),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
+                        color: isToday ? Colors.red : Colors.black,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showForm = true; // Setelah tombol ditekan, tampilkan form
+                  });
+                },
+                child: Icon(Icons.add),
+              ),
+            ),
+            SizedBox(height: 20),
+            if (showForm)
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      _getMonthName(date.month),
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Form Belanja',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              showForm = false; // Saat tombol 'Kembali' ditekan, sembunyikan form
+                            });
+                          },
+                          child: Text('Kembali'),
+                        ),
+                      ],
                     ),
-                    Text(
-                      '${date.year}',
-                      style: TextStyle(fontSize: 18),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Nama Barang'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Nama Barang'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Nama Barang'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Nama Barang'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Logika untuk menyimpan data dari form
+                      },
+                      child: Text('Simpan'),
                     ),
                   ],
                 ),
               ),
-              Expanded(child: _buildMonthView(date)),
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  String _getMonthName(int month) {
-    List<String> monthNames = [
-      'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
-      'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
-    ];
-    return monthNames[month - 1];
-  }
-
-  void _showExpenseDetails(int year, int month, int day) {
-    // Implementasi detail belanja berdasarkan tanggal yang diklik
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Detail Belanja'),
-          content: Text('Belanja pada $day/$month/$year'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Tutup'),
-            ),
           ],
-        );
-      },
-    );
-  }
-
-  Widget _buildMonthView(DateTime date) {
-  List<String> daysOfWeek = ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'];
-  List<Widget> dayWidgets = [];
-
-  // Add day of week headers
-  for (String day in daysOfWeek) {
-    dayWidgets.add(
-      Container(
-        alignment: Alignment.center,
-        child: Text(
-          day,
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
         ),
       ),
     );
   }
 
-  // Add empty containers for days before the 1st
-  int firstWeekday = DateTime(date.year, date.month, 1).weekday - 1;
-  for (int i = 0; i < firstWeekday; i++) {
-    dayWidgets.add(Container());
+  int daysInMonth(DateTime date) {
+    var firstDayThisMonth = DateTime(date.year, date.month, 1);
+    var firstDayNextMonth = DateTime(firstDayThisMonth.year, firstDayThisMonth.month + 1, 1);
+    return firstDayNextMonth.subtract(Duration(days: 1)).day;
   }
+}
 
-  // Add dates
-  int daysInMonth = DateTime(date.year, date.month + 1, 0).day;
-  for (int i = 1; i <= daysInMonth; i++) {
-    bool isToday = date.year == DateTime.now().year &&
-        date.month == DateTime.now().month &&
-        i == DateTime.now().day;
 
-    dayWidgets.add(
-      GestureDetector(
-        onTap: () {
-          _showExpenseDetails(date.year, date.month, i);
-        },
-        child: Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.all(2.0),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            color: isToday ? Colors.red.shade100 : Colors.transparent,
-          ),
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    '$i',
-                    style: TextStyle(color: isToday ? Colors.red : Colors.green),
-                  ),
-                ),
-              ),
-              if (i == 11 || i == 7) // Contoh untuk menambahkan ikon bulat
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: 24.0,
-                    height: 24.0,
-                    decoration: BoxDecoration(
-                      color: i == 11 ? Colors.red : Colors.blue,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '1',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  return GridView.count(
-    crossAxisCount: 7,
-    childAspectRatio: 0.8,
-    children: dayWidgets,
-  );
-}}
